a0b0489c2de7df4bdee6bd831bd2abdaf874b8cf
ahorvath12 <ahorvath@scu.edu>
2021-04-15 15:46:54 -0700

    post processing

--- Assets/Scripts/MovementRecognizer.cs
+++ Assets/Scripts/MovementRecognizer.cs
@@ -52,7 +52,8 @@ public class MovementRecognizer : MonoBehaviour
     {
         //isPressed = inputProcessor.rightHandTrigger;
         InputHelpers.IsPressed(InputDevices.GetDeviceAtXRNode(inputSource), inputButton, out bool isPressed, inputThreshold);
-        
+
+        Debug.Log(isPressed);
 
         //start the movement
         if(!isMoving && isPressed)
